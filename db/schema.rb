998fd1ab88992c7cb82f7a4de4ded6359c604c30
DataNick <DataNick@users.noreply.github.com>
2015-06-26 00:52:21 -0400

    Generated migration to add user name to users table and ran rake:db migrate to update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150626042819) do
+ActiveRecord::Schema.define(version: 20150626045016) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                           null: false
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20150626042819) do
     t.string   "reset_password_token"
     t.datetime "reset_password_token_expires_at"
     t.datetime "reset_password_email_sent_at"
+    t.string   "user_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
