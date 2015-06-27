4244a30c123994fc2e06e22a36e587ad86b33e6f
DataNick <DataNick@users.noreply.github.com>
2015-06-27 01:35:25 -0400

    Updated actions in projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,14 +1,32 @@
 class ProjectsController < ApplicationController
   def index
-  	@projects = Project.all
+  	@projects = if params[:search]
+  		Project.where("name LIKE ?", "%#{params[:search]}%")
+  	else
+  		Project.order('products.created_at DESC').page(params[:page])
+  	end
+  	@projects = @projects.page(params[:page])
+
+  	respond_to do |format|
+  		format.html
+  	end
   end
 
   def show
+  	@project = Project.find(params[:id])
   end
 
   def new
+  	@project = Project.new
   end
 
   def edit
+  	@project = Project.find(params[:id])
+  end
+
+  private
+
+  def project_params
+  	params.require(:project).permit(:title, :description, :goal, :start_date, :end_date)
   end
 end
