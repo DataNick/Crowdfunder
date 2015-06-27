a133d6eaeaecfab2574d5cad603d716fa332bcd6
DataNick <DataNick@users.noreply.github.com>
2015-06-27 01:14:11 -0400

    Generate controllers for welcome, rewards, users and projects

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,25 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/new'
+
+  get 'users/show'
+
+  get 'projects/index'
+
+  get 'projects/show'
+
+  get 'projects/new'
+
+  get 'projects/edit'
+
+  get 'welcome/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
