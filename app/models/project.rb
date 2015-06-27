84a9f8f3aa06fddf218247b0ef1f8b4b44cec35e
DataNick <DataNick@users.noreply.github.com>
2015-06-26 23:24:13 -0400

    Add foreign key owner_id to projects table

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,4 @@
 class Project < ActiveRecord::Base
 	belongs_to :owner, class_name: "User"
+	has_many :rewards
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150627024250) do
+ActiveRecord::Schema.define(version: 20150627031705) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20150627024250) do
     t.datetime "end_date"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "owner_id"
   end
 
   create_table "rewards", force: :cascade do |t|
