4d3077ff9f632bf13c49e396e93ba609dda64aa3
DataNick <DataNick@users.noreply.github.com>
2015-06-27 01:22:39 -0400

    Updated actions in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,28 @@
 class UsersController < ApplicationController
   def index
+  	@users = User.all
   end
 
   def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(user_params)
+  		if @user.save
+  			redirect_to products_url, notice: "Signed Up!"
+  		else
+  			render "new"
+  		end
   end
 
   def show
+  	@user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+  	params.require(:user).permit(:email, :name, :password, :password_confirmation)
   end
 end
