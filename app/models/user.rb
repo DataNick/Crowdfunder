3349ffea868beb4e93589cd2a75a07490dbf8308
DataNick <DataNick@users.noreply.github.com>
2015-06-28 00:16:12 -0400

    Added foreign_key to association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
-  has_many :projects
+  has_many :projects, foreign_key: "owner_id"
 
   validates :password, confirmation: true
   validates :password_confirmation, presence: true
